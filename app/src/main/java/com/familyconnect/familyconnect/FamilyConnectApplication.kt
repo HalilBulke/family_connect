699a89dac1ef4b3ac2d74270489fc9f8941075be
Halil Bulke <halil.bulke@commencis.com>
2024-01-22 23:33:29 +0300

    Add Family Connect Application

---
+++ app/src/main/java/com/familyconnect/familyconnect/FamilyConnectApplication.kt
@@ -0,0 +1,8 @@
+package com.familyconnect.familyconnect
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FamilyConnectApplication: Application() {
+}
